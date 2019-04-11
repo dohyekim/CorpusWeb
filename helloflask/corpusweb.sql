4cda0c9815a20d757322067d4e627a01aa213465
dohyekim <dhleen5@hanmail.net>
2019-04-11 17:59:30 +0900

    project

--- helloflask/corpusweb.sql
+++ helloflask/corpusweb.sql
@@ -10,16 +10,16 @@ create table User(
 select * from User;
 /*truncate table Post; */
 select * from Post;
-drop table Post;
+/*drop table Post;*/
+
 CREATE TABLE `Post` (
-  `postid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `postid` int(10) unsigned NOT NULL,
   `title` varchar(256),
   `date_posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` text NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`postid`)
+  `user_id` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-insert into Post(title, content, user_id) values('글1', '이것은 글입니다', 1);
-insert into Post(title, content, user_id) values('글2', '이것은 글2입니다', 1);
-insert into Post(title, content, user_id) values('글3', '이것은 글2입니다', 1);
+insert into Post(postid,title, content, user_id) values(1,'글1', '이것은 글입니다', 1);
+insert into Post(postid,title, content, user_id) values(2,'글2', '이것은 글2입니다', 1);
+insert into Post(postid,title, content, user_id) values(3,'글3', '이것은 글2입니다', 1);
